03eb888c2ef41e5f0b6d0a04879b00cc4ed2f5d7
Tatyana Lozhkova <cypress-girl@mail.ru>
2020-10-08 23:19:35 +0300

    Created begining structure of the application.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import BlogPage from "./pages/BlogPage";
+import ContactsPage from "./pages/ContactsPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className=".app-wrapper">
+                <header>
+                    <div className="menu-header">
+                        <Link to="/">Home</Link>
+                        <Link to="/projects">Проекты</Link>
+                        <Link to="/blog">Блог</Link>
+                        <Link to="/contacts">Контакты</Link>
+                    </div>
+                </header>
+                <Switch>
+                    <Route exact={true} path="/">
+                        <HomePage/>
+                    </Route>
+                    <Route exact={true} path="/projects">
+                        <ProjectsPage/>
+                    </Route>
+                    <Route exact={true} path="/blog">
+                        <BlogPage/>
+                    </Route>
+                    <Route exact={true} path="/contacts">
+                        <ContactsPage/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
